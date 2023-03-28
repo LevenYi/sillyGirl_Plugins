26b880e4eb8f1cc0fae45f9e5148a923477add35
Leven_Yi <leven_yi@qq.com>
2023-03-28 21:52:51 +0800

    update

--- download/16dbd882-32f7-11ed-89fb-fc7c02eb3d87.js
+++ download/16dbd882-32f7-11ed-89fb-fc7c02eb3d87.js
@@ -139,9 +139,9 @@ function main(){
 			// let pins=st.GetBind(s.getPlatform(),s.getUserId())	//获取用户已绑定的pin
 			// let pin=result.match(/(?<=pin=)[^;]+/)
 			// if(pins.indexOf(pin)!=-1)
-			// 	sillyGirl.notifyMasters("报告老板！[ "+pin+" ]更新账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式:nark短信")
+			// 	sillyGirl.notifyMasters("报告老板！[ "+pin+" ]更新账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式：nark短信")
 			// else
-			// 	sillyGirl.notifyMasters("报告老板！[ "+pin+" ]添加账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式:nark短信")			
+			// 	sillyGirl.notifyMasters("报告老板！[ "+pin+" ]添加账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式：nark短信")			
 			// UpdateUserData(pin)	//更新账号数据	
 			
 			s.setContent(result)
@@ -183,9 +183,9 @@ function main(){
 		else
 			pin=result
 		if(pins.indexOf(pin)!=-1)
-			sillyGirl.notifyMasters("报告老板！[ "+pin+" ]更新账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式:nolanPro扫码")
+			sillyGirl.notifyMasters("报告老板！[ "+pin+" ]更新账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式：nolanPro扫码")
 		else
-			sillyGirl.notifyMasters("报告老板！[ "+pin+" ]添加账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式:nolanPro扫码")			
+			sillyGirl.notifyMasters("报告老板！[ "+pin+" ]添加账号\n绑定客户："+s.getUserId()+"("+s.getPlatform()+")\n登陆方式：nolanPro扫码")			
 		UpdateUserData(pin)	//更新账号数据
         s.reply("登陆成功，账号更新中...\n请等待几分钟后再查询账号信息")
 	
